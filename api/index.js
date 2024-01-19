b2cba7df6c538bfffbaa2f3e812b406cbbf09c4a
dipu-kr <dipum906@gmail.com>
2024-01-19 22:57:37 +0530

    connect mongodb and create user schema

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => {
+    console.log("Mongodb connected!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello api");
 });
-app.listen(5000, () => {
-  console.log("server is running on port:5000");
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port: ${process.env.PORT}`);
 });
